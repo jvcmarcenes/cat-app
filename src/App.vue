527bc5691db28a20e09834b9935f630b3cf07a9a
Joao Vitor Costa Marcenes <jvcmv.jvcmarcenes@gmail.com>
2020-04-09 11:13:30 -0300

    skeleton done

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,177 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <header>
+      <h1>
+        Yako
+        <FaIcon icon="cat"/>
+      </h1>
+    </header>
+
+    <div class="container">
+
+      <div class="image-container">
+        <div v-if="state === 'loading'" class="loader" />
+        <img v-else :src="image">
+      </div>
+
+      <button class="btn" @click="loadNextImage">
+        <span class="btn-label">Random Yako</span>
+        <FaIcon icon="sync-alt" />
+      </button>
+    </div>
+
+    <footer>
+      <span class="credits-author">An app by Joao Vitor Marcenes</span>
+      <span class="credits-images">Images by Aden Forhsaw / theCatApi</span>
+    </footer>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import axios from 'axios'
+  import { catKey } from './keys'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  import { library } from '@fortawesome/fontawesome-svg-core'
+  import { faCat, faSyncAlt } from '@fortawesome/free-solid-svg-icons'
+  import { FontAwesomeIcon as FaIcon } from '@fortawesome/vue-fontawesome'
+  library.add(faCat, faSyncAlt)
+
+  export default {
+    name: 'App',
+    components: {
+      FaIcon
+    },
+    data() {
+      return {
+        state: "",
+        image: ""
+      }
+    },
+    created() {
+      this.loadNextImage()
+    },
+    methods: {
+      async loadNextImage() {
+        try {
+          axios.defaults.headers.common['x-api-key'] = catKey
+
+          const params = {
+            limit: 1,
+            size: 'full'
+          }
+
+          this.state = "loading"
+          const res = await axios.get('https://api.thecatapi.com/v1/images/search', { params } )
+          this.state = "loaded"
+
+          this.image = res.data[0].url
+
+        } catch (err) {
+          console.log(err)
+        }
+      }
+    }
   }
-}
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  * {
+    margin: 0px;
+    padding: 0px;
+    box-sizing: border-box;
+  }
+
+  #app {
+    font-family: Arial, Helvetica, sans-serif;
+  }
+
+  @keyframes spin {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+
+  .loader {
+    width: 120px;
+    height: 120px;
+
+    border: 16px #f3f3f3 solid;
+    border-top: 16px #3498db solid;
+    border-radius: 50%;
+
+    animation: spin 2s ease-in-out infinite;
+  }
+
+  header {
+    padding: 20px;
+
+    background: #54f;
+    color: #f2f2f2;
+
+    text-align: center;
+
+    font-size: 2rem;
+  }
+
+  .container {
+    padding: 30px 0px;
+    margin: auto;
+
+    width: 80vw;
+  }
+
+  .image-container {
+    display: flex;
+
+    margin-bottom: 15px;
+
+    width: 100%;
+    height: 80vw;
+
+    justify-content: center;
+    align-items: center;
+
+    >img {
+      flex-shrink: 0;
+      max-width: 100%;
+      max-height: 100%;
+
+      background: black;
+    }
+  }
+
+  .btn {
+    padding: 10px;
+
+    width: 100%;
+
+    background: #54f;
+    color: #f2f2f2;
+
+    border: none;
+    outline: none;
+
+    font-size: 1rem;
+    font-weight: bold;
+
+    cursor: pointer;
+
+    &-label {
+      margin-right: 10px;
+    }
+
+    &:hover {
+      background: lighten(#54f, 5%);
+    }
+  }
+
+  footer {
+
+  }
+
 </style>
---
+++ src/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  catKey: "dfaea3e5-870b-4b97-a4d7-0612f6520246"
+}
